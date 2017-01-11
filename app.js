c8758da29b68fe36900d6323bba651454ae0deac
LuisVilches <lvilches21@gmail.com>
2017-01-11 18:53:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -47,20 +47,15 @@ app.post('/saldo',function(req,res){
 app.post('/tbeep',function(req,res){
 	var string = req.body.trigger_word;
 	var cm = string.split(" ");
-	if (cm[1] = "dowhi"){
-	//res.json({text: "me pase po!"});
+	if (cm[1] = ""){
 		res.json({text: "me pase po!!!!"});
-	}
+	};
+	if (cm[1] = "dowhi"){
+		res.json({text: "Dime?"});
+	};
 	if (cm[1] = "hola"){
-	//res.json({text: "me pase po!"});
 		res.json({text: "Hola estimado!"});
-	}
-	
-	//if (cm[1] == "saldo"){
-	//	res.json({text: "tu saldo es:  $1200"});
-	//}else{
-	//	res.json({text: "hola"});
-	//}
+	};
 });
 
 
